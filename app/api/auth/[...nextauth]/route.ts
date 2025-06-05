7d8ed52af8d72894c8664601d8dde2f617cb412e
Daniel-Rafique <d.rafique@icloud.com>
2025-06-05 10:54:03 +0100

    update callback

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -17,10 +17,10 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   }))
 }
 
-if (process.env.GITHUB_ID && process.env.GITHUB_SECRET) {
+if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
   providers.push(GitHubProvider({
-    clientId: process.env.GITHUB_ID,
-    clientSecret: process.env.GITHUB_SECRET,
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
   }))
 }
 
@@ -62,6 +62,8 @@ providers.push(CredentialsProvider({
         email: user.email,
         name: user.name,
         image: user.avatar || undefined,
+        type: user.type || "CONSUMER",
+        verified: user.emailVerified !== null
       }
     } catch (error) {
       console.error("Error during credentials authorization:", error)
